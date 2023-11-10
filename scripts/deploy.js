315e33ac3cac6a99c738a8156454480d21964e86
Edidiong <ephraimedy@gmail.com>
2023-11-10 15:08:08 +0100

    Made some changes to the deploy file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,31 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+// Imports
+// Imported ethers from hardhat rather than independently
+const { ethers } = require("hardhat");
 
+
+// This function will hold most of the neccessary interactivity
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  // Defining the contract factory that would be used to deploy the contract
+  const contractfactory = await ethers.getContractFactory(
+    "SimpleStorage"
   );
-}
+  console.log("Deploying...")
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  // Actual deploying of the contract
+  const contract = await contractfactory.deploy();
+  
+  // Transaction receipt
+  const tx = await contract.deploymentTransaction();
+  console.log( await contract.getAddress())
+  
+};
+
+main()
+.then(() => {process.exit(0)})
+.catch((err) => {
+  console.log(err);
+  process.exit(1)
+})
+
+
