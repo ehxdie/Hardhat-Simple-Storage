aebaa29016a39c0cf721724125fb1080bd5967dc
Edidiong <ephraimedy@gmail.com>
2023-11-14 11:23:43 +0100

    Made some little changes to the block-number

--- tasks/block-number.js
+++ tasks/block-number.js
@@ -5,6 +5,9 @@ task("block-number", "Prints the current block number").setAction(
     async (taskArgs, hre) => {
         // Getting the current block number 
         const blockNumber = await hre.ethers.provider.getBlockNumber();
-        
+        console.log(blockNumber);
+
     }
 )
+
+module.exports = { }
